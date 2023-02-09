a4e8958cbd8432615e4b4eb3b35a385dbf12ed0b
san <syedsaniaa0a@gmail.com>
2023-02-09 22:04:58 +0530

    My new commit

---
+++ new1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("new file has been created");
+    return 0;
+}
---
+++ new2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("2nd new file has been created");
+    return 0;
+}
